7c8d90e4c426601ff576f1eca39ef354d929e6f4
alblinrey <mveebb@gmail.com>
2025-09-17 14:21:43 +0200

    Initial commit

---
+++ spring-todo/src/main/java/com/proyectotodo/spring_todo/SpringTodoApplication.java
@@ -0,0 +1,13 @@
+package com.proyectotodo.spring_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoApplication.class, args);
+	}
+
+}
